52c4a6e25552c812b78e013a815b3ac3640c7812
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2023-05-20 14:46:31 +0100

    updated contact details part of the webpage, and set up form for API post request

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,255 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Contact() {
-  return <h1>Contact us</h1>;
+  const [stateObj, setStateObj] = useState({
+    fullName: "",
+    emailAddress: "",
+    phoneNum: "",
+    phoneNum2: "",
+    message: "",
+    address1: "",
+    address2: "",
+    city: "",
+    stateCounty: "",
+    postcode: "",
+    country: "",
+  });
+
+  const [phone2isOn, setPhone2isOn] = useState(false);
+  const [addressIsOn, setAddressIsOn] = useState(false);
+
+  function dataChecks(formData) {
+    let errorMessage = "";
+    let validDetails = true;
+
+    if (!formData.fullName) {
+      errorMessage += "enter your full name \n";
+      validDetails = false;
+    }
+
+    if (
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+        formData.emailAddress
+      )
+    ) {
+      errorMessage += "enter a valid email \n";
+      validDetails = false;
+    }
+
+    if (formData.phoneNum.length > 0) {
+      if (!/^\+?[0-9]{1,20}$/.test(formData.phoneNum)) {
+        errorMessage += "enter a valid phone number \n";
+        validDetails = false;
+      }
+    }
+
+    if (formData.phoneNum2.length > 0) {
+      if (!/^\+?[0-9]{1,20}$/.test(formData.phoneNum)) {
+        errorMessage += "enter a valid (second) phone number \n";
+        validDetails = false;
+      }
+    }
+
+    if (formData.message.length === 0) {
+      errorMessage += "enter a message \n";
+      validDetails = false;
+    } else if (formData.message.length > 500) {
+      errorMessage += "enter a message under 500 characters \n";
+      validDetails = false;
+    }
+
+    if (addressIsOn) {
+      if (formData.address1 === "") {
+        errorMessage += "enter address line 1 \n";
+        validDetails = false;
+      }
+
+      if (formData.city === "") {
+        errorMessage += "enter a city/town \n";
+        validDetails = false;
+      }
+
+      if (formData.stateCounty === "") {
+        errorMessage += "enter a state/county \n";
+        validDetails = false;
+      }
+
+      if (
+        !/^[A-Z]{1,2}[0-9RCHNQ][0-9A-Z]?\s?[0-9][ABD-HJLNP-UW-Z]{2}$|^[A-Z]{2}-?[0-9]{4}$/.test(
+          formData.postcode
+        )
+      ) {
+        errorMessage += "enter a valid postcode \n";
+        validDetails = false;
+      }
+
+      if (formData.country === "") {
+        errorMessage += "enter a state/county \n";
+        validDetails = false;
+      }
+    }
+
+    return [errorMessage, validDetails];
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const [errorMessage, validDetails] = dataChecks(stateObj);
+    if (!validDetails) {
+      alert(errorMessage);
+    } else {
+      const requestObj = {
+        FullName: stateObj.fullName,
+        EmailAddress: stateObj.emailAddress,
+        PhoneNumbers: [stateObj.phoneNum, stateObj.phoneNum2],
+        Message: stateObj.message,
+        bIncludeAddressDetails: addressIsOn,
+        AddressDetails: {
+          AddressLine1: stateObj.address1,
+          AddressLine2: stateObj.address2,
+          CityTown: stateObj.city,
+          StateCounty: stateObj.stateCounty,
+          Postcode: stateObj.postcode,
+          Country: stateObj.country,
+        },
+      };
+    }
+  }
+
+  function handleChange(e) {
+    const newObject = { ...stateObj };
+    newObject[e.target.name] = e.target.value;
+    setStateObj(newObject);
+  }
+
+  useEffect(() => {}, [phone2isOn, addressIsOn]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Full name:{" "}
+        <input
+          type="text"
+          value={stateObj.fullName}
+          onChange={(e) => handleChange(e)}
+          name="fullName"
+        />
+      </label>
+      <label>
+        Email address:{" "}
+        <input
+          type="text"
+          value={stateObj.emailAddress}
+          onChange={(e) => handleChange(e)}
+          name="emailAddress"
+        />
+      </label>
+      <label>
+        Phone number 01 (optional):{" "}
+        <input
+          type="text"
+          value={stateObj.phoneNum}
+          onChange={(e) => handleChange(e)}
+          name="phoneNum"
+        />
+      </label>
+      <input
+        value="phone number 2?"
+        type="button"
+        onClick={() => {
+          setPhone2isOn(!phone2isOn);
+        }}
+      />
+      {phone2isOn && (
+        <label>
+          Phone number 02 (optional):{" "}
+          <input
+            type="text"
+            value={stateObj.phoneNum2}
+            onChange={(e) => handleChange(e)}
+            name="phoneNum2"
+          />
+        </label>
+      )}
+      <label>
+        Message:{" "}
+        <input
+          type="text"
+          value={stateObj.message}
+          onChange={(e) => handleChange(e)}
+          name="message"
+        />
+      </label>
+
+      <input
+        value="address"
+        type="button"
+        onClick={() => {
+          setAddressIsOn(!addressIsOn);
+        }}
+      />
+
+      {addressIsOn && (
+        <div>
+          <label>
+            Address line 1:{" "}
+            <input
+              type="text"
+              value={stateObj.address1}
+              onChange={(e) => handleChange(e)}
+              name="address1"
+            />
+          </label>
+          <label>
+            Address line 2:{" "}
+            <input
+              type="text"
+              value={stateObj.address2}
+              onChange={(e) => handleChange(e)}
+              name="address2"
+            />
+          </label>
+          <label>
+            City/Town:{" "}
+            <input
+              type="text"
+              value={stateObj.city}
+              onChange={(e) => handleChange(e)}
+              name="city"
+            />
+          </label>
+          <label>
+            State/County:{" "}
+            <input
+              type="text"
+              value={stateObj.stateCounty}
+              onChange={(e) => handleChange(e)}
+              name="stateCounty"
+            />
+          </label>
+          <label>
+            Postcode:{" "}
+            <input
+              type="text"
+              value={stateObj.postcode}
+              onChange={(e) => handleChange(e)}
+              name="postcode"
+            />
+          </label>
+          <label>
+            Country:{" "}
+            <input
+              type="text"
+              value={stateObj.country}
+              onChange={(e) => handleChange(e)}
+              name="country"
+            />
+          </label>
+        </div>
+      )}
+      <label>
+        <input type="submit" name="submit" />
+      </label>
+    </form>
+  );
 }
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from "react";
+import { LoremIpsum } from "lorem-ipsum";
 
 export default function Homepage() {
   const [homeData, setHomeData] = useState([]);
+  const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4,
+    },
+    wordsPerSentence: {
+      max: 16,
+      min: 4,
+    },
+  });
 
   useEffect(() => {
     fetch(
@@ -14,11 +25,23 @@ export default function Homepage() {
         console.log(data.Details);
         const displayData = data.Details.map((item) => {
           return (
-            <li>
-              <h1>{item.Title}</h1>
-              <h1>{item.Subtitle}</h1>
-              <img src={item.ImageUrl} class="homepageImage" />
-            </li>
+            <div>
+              <li>
+                <h1>{item.Title}</h1>
+                <h1>{item.Subtitle}</h1>
+                <img src={item.ImageUrl} class="homepageImage" />
+              </li>
+              <div>
+                <h3>{lorem.generateSentences(2)}</h3>
+                <p>{lorem.generateParagraphs(2)}</p>
+                <p>{lorem.generateParagraphs(2)}</p>
+              </div>
+              <div>
+                <h3>{lorem.generateSentences(2)}</h3>
+                <p>{lorem.generateParagraphs(2)}</p>
+                <p>{lorem.generateParagraphs(2)}</p>
+              </div>
+            </div>
           );
         });
         console.log(Array.isArray(displayData));
